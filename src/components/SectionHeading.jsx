701c8514c7f30f8f61e5dbb26e556893704a0876
aliasrafbd <ps.aliasraf@gmail.com>
2025-01-28 22:57:15 +0600

    final commit

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -5,7 +5,6 @@ const SectionHeading = ({title, subtitle}) => {
 
         <div>
             <h1 className='text-center text-3xl font-bold my-12'>{title}</h1>
-            <hr className='my-4 max-w-3xl mx-auto border-slate-400 border-2' />
             <h1>{subtitle}</h1>
         </div>
     );
